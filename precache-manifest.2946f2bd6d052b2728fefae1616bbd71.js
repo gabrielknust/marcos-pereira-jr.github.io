7a41a9af89d2b170bcaecc10bf98f078494a127e
marcos-pereira-jr <marcos.ant.pereira.jr@gmail.com>
2018-11-27 18:08:59 -0200

    ultimo

---
+++ precache-manifest.2946f2bd6d052b2728fefae1616bbd71.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5976e03172dfbb46054b5b9ca49380f3",
+    "url": "/static/media/peixe.5976e031.svg"
+  },
+  {
+    "revision": "279810fcea6dc65ee117",
+    "url": "/static/css/main.4953a6eb.chunk.css"
+  },
+  {
+    "revision": "10659db9720a00944b7b",
+    "url": "/static/js/1.10659db9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7b87a0473512c46faea51c73ea207b1",
+    "url": "/static/media/biology.a7b87a04.svg"
+  },
+  {
+    "revision": "bb566d48c0afa24d7c8194c7bb09cbf0",
+    "url": "/static/media/passaro.bb566d48.svg"
+  },
+  {
+    "revision": "279810fcea6dc65ee117",
+    "url": "/static/js/main.279810fc.chunk.js"
+  },
+  {
+    "revision": "2fbb2ccf44c7dc0629d1bd16a8bcba51",
+    "url": "/static/media/bicho.2fbb2ccf.svg"
+  },
+  {
+    "revision": "d99d1e1c62b3e37555a133b591d93cbd",
+    "url": "/static/media/planta.d99d1e1c.svg"
+  },
+  {
+    "revision": "953ef8c3e7a09e53412a6afaf0052eb7",
+    "url": "/static/media/jacare.953ef8c3.svg"
+  },
+  {
+    "revision": "063849b3a083567071b6338c8d7badb9",
+    "url": "/static/media/ramo.063849b3.svg"
+  },
+  {
+    "revision": "a0c0e169191259b940bd15d205b89d0e",
+    "url": "/static/media/formiga.a0c0e169.svg"
+  },
+  {
+    "revision": "8908c55ba6e3a7e6b44d783fd8bade05",
+    "url": "/index.html"
+  }
+];
